0eba15c63044020823d7207f6bbcf769e392e5be
CodeRadu <47945947+CodeRadu@users.noreply.github.com>
2024-05-01 16:59:16 +0300

    Add more branding

--- post-install.sh
+++ post-install.sh
@@ -2,10 +2,20 @@
 
 set -ouex pipefail
 
-# Set os name and color
+# Set os-release and color
 
 sed -i 's/Fedora Linux/RaduLinux/g' /etc/os-release
 sed -i 's/ANSI_COLOR="[^"]*"/ANSI_COLOR="0;38;2;205;0;0"/' /etc/os-release # RGB 205 0 0
+sed -i 's/ID=fedora/ID=radulinux/g' /etc/os-release
+sed -i 's/CPE_NAME="[^"]*"//' /etc/os-release
+sed -i 's/DEFAULT_HOSTNAME="[^"]*"/DEFAULT_HOSTNAME="radulinux"/' /etc/os-release
+sed -i 's/HOME_URL="[^"]*"/HOME_URL="https:\/\/github.com\/RaduLinux\/RaduLinux"/' /etc/os-release
+sed -i 's/DOCUMENTATION_URL="[^"]*"//' /etc/os-release
+sed -i 's/SUPPORT_URL="[^"]*"//' /etc/os-release
+sed -i 's/BUG_REPORT_URL="[^"]*"/BUG_REPORT_URL="https:\/\/github.com\/RaduLinux\/RaduLinux\/issues"/' /etc/os-release
+sed -i 's/REDHAT_.*//g' /etc/os-release
+
+sed -i '/^$/d' /etc/os-release
 
 # Generate initramfs
 
